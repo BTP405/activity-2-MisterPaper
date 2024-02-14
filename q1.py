85cbe8d8d9e455ff0acdadd91b0b145ed73338b0
Adiel <adielroytgarts@gmail.com>
2024-02-14 14:21:34 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,14 @@
+def checkIsPrime(n):
+    isPrime = True
+    i = 2
+    while i <= (n**0.5):
+        if n % i == 0:
+            isPrime = False
+        i+=1
+
+    return isPrime
+
+
 def getPrimeNumbers(n):
+    primes = {x for x in range(n+1) if checkIsPrime(x)}
+    return primes
